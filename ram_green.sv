2e401fa29537e2d26d5e2a73d471cfc5ad2dee40
kushpatel0703 <kushpatel0703@gmail.com>
2019-04-19 21:40:23 -0500

    the rest of VGA Sprites, not working yet, but it's going

---
+++ ram_green.sv
@@ -0,0 +1,28 @@
+/*
+ * ECE385-HelperTools/PNG-To-Txt
+ * Author: Rishi Thakkar
+ *
+ */
+
+module  frameRAM_green
+(
+		input [18:0] read_address,
+		input Clk,
+
+		output logic [23:0] data_Out
+);
+
+// mem has width of 24 bits and a total of 4096 addresses
+logic [23:0] mem [0:4095];
+
+initial
+begin
+	 $readmemh("sprite_bytes/green.txt", mem);
+end
+
+
+always_ff @ (posedge Clk) begin
+	data_Out<= mem[read_address];
+end
+
+endmodule
